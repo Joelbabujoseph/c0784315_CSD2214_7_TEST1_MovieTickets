ff5e2fe421728a8bcaa49a56b1751808ce1927c4
Joelbabujoseph <joelbabujoseph@gmail.com>
2021-03-11 21:54:53 -0500

    initial commit

--- movie.js
+++ movie.js
@@ -3,4 +3,14 @@ const seats=document.querySelectorAll('.row .seat:not(.occupied)');
 const count=document.getElementById('count');
 const price=document.getElementById('amount');
 
-
+const movieSelect=document.getElementById('film');
+let ticketprice=+movieSelect.value;
+const populateUI=()=>{
+    const selectedSeats=JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats!= null&&selectedSeats.length>0){
+        seats.forEach((seat,index)=>{
+            if(selectedSeats.indexOf(index)>-1){
+                seat.classList.add('selected');
+            }
+        });
+    }
