e5dd229b4b3ce5a4a192efc733ef5dcd8005e7a3
Joelbabujoseph <joelbabujoseph@gmail.com>
2021-03-11 21:47:28 -0500

    initial commit

--- movie.js
+++ movie.js
@@ -1,10 +1,6 @@
-const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-const count = document.getElementById('count');
-const amount = document.getElementById('amount');
-
-const movieselect = document.getElementById('film');
-let ticketprice = +movieselect.Value;
-
+const container=document.querySelector('.container');
+const seats=document.querySelectorAll('.row .seat:not(.occupied)');
+const count=document.getElementById('count');
+const price=document.getElementById('amount');
 
 
